ec7fd24cfe028eb038b35da65ad6749ac130d2aa
Nabed Khan <nahidcoder@gmail.com>
2025-11-22 18:06:26 +0600

    Implement function to duplicate zeros in an array.

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func duplicateZeros(arr []int) {
+	zeroCount := 0
+	lastIndex := len(arr) - 1
+
+	for i := 0; i <= lastIndex-zeroCount; i++ {
+		if arr[i] == 0 {
+			if i == lastIndex-zeroCount {
+				arr[lastIndex] = 0
+				lastIndex--
+				break
+			}
+
+			zeroCount++
+		}
+	}
+
+	newLastIndex := lastIndex - zeroCount
+
+	for j := newLastIndex; j >= 0; j-- {
+		if arr[j] == 0 {
+			arr[j+zeroCount] = 0
+			zeroCount--
+			arr[j+zeroCount] = 0
+		} else {
+			arr[j+zeroCount] = arr[j]
+		}
+	}
+
+	fmt.Println(lastIndex, zeroCount)
+	fmt.Println(arr)
+}
+
+func main() {
+	numbers := []int{1, 0, 2, 3, 0, 0, 5, 6}
+	duplicateZeros(numbers)
+}
